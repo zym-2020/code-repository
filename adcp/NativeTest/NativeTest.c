32f7f9724cdcc892cda39b370f2fe4fc32b8562c
zym-2020 <1161384816@qq.com>
2023-05-11 16:25:53 +0800

    开源的力量！！！

---
+++ adcp/NativeTest/NativeTest.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "edu_njnu_JNITest.h"
+
+JNIEXPORT jint JNICALL Java_edu_njnu_JNITest_sum
+(JNIEnv* env, jobject obj, jint x, jint y) {
+	return x + y;
+}
---
+++ adcp/dolfyn-demo/adcp-demo.py
@@ -0,0 +1,7 @@
+import dolfyn
+from dolfyn.adp import api
+
+ds = dolfyn.read(
+    './data/SAVE2023_4_19_9-27-03.DAT')
+
+print(ds)
