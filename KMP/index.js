c14c81d5fd431d04fca4d056251bc4a80cd57609
zym-2020 <1161384816@qq.com>
2023-05-25 10:54:18 +0800

    no msg

---
+++ KMP/index.js
@@ -0,0 +1,30 @@
+const getNext = (s) => {
+  const next = new Array(s.length).fill(-1);
+  let k = -1,
+    i = 0;
+  while (i < s.length - 1) {
+    if (k === -1 || s[i] === s[k]) {
+      i++;
+      k++;
+      next[i] = k;
+    } else k = next[k];
+  }
+  return next;
+};
+
+const KMP = (s1, s2) => {
+  const next = getNext(s2);
+  let p1 = 0,
+    p2 = 0;
+  while (p1 < s1.length && p2 < s2.length) {
+    if (p2 === -1 || s1[p1] === s2[p2]) {
+      p1++;
+      p2++;
+    } else {
+      p2 = next[p2];
+    }
+  }
+  if (p2 < s2.length) return false;
+  else return true;
+};
+console.log(KMP("abcdabe", "abf"));
--- adcp/dolfyn-demo/adcp-demo.py
+++ adcp/dolfyn-demo/adcp-demo.py
@@ -2,17 +2,21 @@ import dolfyn
 from dolfyn.adp import api
 
 ds = dolfyn.read(
-    './data/SAVE2023_4_19_9-27-03.DAT')
-
-# print(ds)
-
-print(ds.dims)
-print(ds.coords)
-print(ds.attrs)
-print(ds.data_vars)
+    './data/BMS1_D_002.PD0')
+dat_dolfyn = ds.velds
+print(ds)
 print("########################")
-print(len(ds))
-for key in ds.keys():
-    print(type(ds[key]))
+
+# print(ds.dims)
+# print(ds.coords)
+# print(ds.attrs)
+print(ds.data_vars)
+
+print(ds.data_vars['vel_bt'][3][1])
+print(len(ds.data_vars['vel_bt'][0]))
+# print("########################")
+# print(len(ds))
+# for key in ds.keys():
+#     print(type(ds[key]))
 # print(len(ds['time']))
 # print(ds['time'])
